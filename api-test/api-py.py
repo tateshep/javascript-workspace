e87d0126ea18a5e80393d3e10809ea2a122ff18e
Tate <tateshep@gmail.com>
2019-07-23 14:00:25 -0600

    working with python api

---
+++ api-test/api-py.py
@@ -0,0 +1,34 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+from secrets import secret_key
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+    'start': '1',
+    'limit': '300',
+    'convert': 'USD'
+}
+
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': secret_key
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    for i in range(0,len(data["data"])):
+        print("\n")
+        print(data["data"][i]["slug"],data["data"][i]["quote"]["USD"]['price'])
+    # print(len(data["data"]))
+    # print(len(data))
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
+
+# for item in data.keys():
+#     print(item)
